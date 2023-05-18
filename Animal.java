30cae7ac526794e9dcb62317d780d1ea9610d8ba
jgower05 <gower.james2019@gmail.com>
2023-05-18 11:52:33 +0100

    Initial commit
    Created a class diagram and initial classes/interfaces

--- Animal.java
+++ Animal.java
@@ -2,4 +2,11 @@ public abstract class Animal
 {
 	private String name;
 	private int age;
+	private Traits traits;
+
+	public abstract Animal reproduce();
+
+	public abstract void eat();
+
+	public abstract void drink();
 } 
---
+++ Traits.java
@@ -0,0 +1,48 @@
+public class Traits
+{
+	private double health;
+	private double stamina;
+	private double speed;
+	private double eyesight;
+	private double intimacy;
+
+	public void setHealth(double x)
+	{
+		this.health = x;
+	}
+
+	public double getHealth(double x)
+	{
+		return this.health;
+	}
+
+	public void setStamina(double x)
+	{
+		this.stamina = x;
+	}
+
+	public double getStamina()
+	{
+		return this.stamina;
+	}
+
+	public void setSpeed(double x)
+	{
+		this.speed = x;
+	}
+
+	public double getSpeed()
+	{
+		return this.speed;
+	}
+
+	public void setEyesight(double x)
+	{
+		this.eyesight = x;
+	}
+
+	public double getEyesight()
+	{
+		return this.eyesight;
+	}
+}
